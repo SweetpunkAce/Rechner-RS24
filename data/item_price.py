38870542c4e1d4c764b02584b6e69f95f86abb1d
SweetpunkAce <ma.le.1999@hotmail.de>
2026-09-11 18:05:39 +0200

    Icons in Datenbank hinterlegt

--- data/item_price.py
+++ data/item_price.py
@@ -1,6 +1,7 @@
 ITEMKATEGORIEN = {
     "endgame_sattel": {
         "bezeichnung": "Endgame-Sattel",
+        "symbol": "assets/icons/items/endgame.png",
         "mindestpreise": {
             "Bauplan": 5000,
             "Konvertierbar": 4000,
@@ -15,6 +16,7 @@ ITEMKATEGORIEN = {
 
     "tek": {
         "bezeichnung": "Tek-Rüstung / Waffen / Werkzeuge",
+        "symbol": "assets/icons/items/tek.png",
         "mindestpreise": {
             "Bauplan": 8000,
             "Konvertierbar": 6000,
@@ -29,6 +31,7 @@ ITEMKATEGORIEN = {
 
     "sonstige_bauplaene": {
         "bezeichnung": "Sonstige Baupläne",
+        "symbol": "assets/icons/items/sonstige.png",
         "mindestpreise": {
             "Bauplan": 1000,
             "Konvertierbar": 0,
